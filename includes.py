00f75a059cdd431f03d9de6cbb600f30403bc3ae
vanduc2514 <vanduc2514@gmail.com>
2024-01-21 22:02:12 +0700

    add support for loading plugins from main configuration

--- includes.py
+++ includes.py
@@ -1,12 +1,14 @@
+import glob
 import os
 import dotbot
 from dotbot.dispatcher import Dispatcher
 from dotbot.cli import read_config
 from dotbot.plugins import Clean, Create, Link, Shell
+from dotbot.util import module
 from argparse import Namespace
 
 
-class Include(dotbot.Plugin):
+class Includes(dotbot.Plugin):
     _directive = "includes"
     _defaults = "defaults"
 
@@ -42,7 +44,6 @@ class Include(dotbot.Plugin):
         else:
             self._log.error("Config for includes must be a string or a dict")
             return False
-        # TODO: merge plugins
         tasks = read_config(self._resolve_path(directory, config_file))
         if not skip_defaults and self._defaults not in tasks:
             # if defaults is not at 0 then other plugins can't find it
@@ -53,13 +54,54 @@ class Include(dotbot.Plugin):
                         directory: str,
                         tasks: dict,
                         options: Namespace):
+        plugins = self._get_plugins(directory, options)
         dispatcher = Dispatcher(base_directory=directory,
                                 only=options.only,
                                 skip=options.skip,
                                 exit_on_failure=options.exit_on_failure,
                                 options=options,
-                                plugins=[Clean, Create, Link, Shell])
+                                plugins=plugins)
         return dispatcher.dispatch(tasks)
 
+    def _get_plugins(self,
+                     directory: str,
+                     options):
+        plugins = []
+        plugin_paths = options.plugins
+        if not options.disable_built_in_plugins:
+            plugins.extend([Clean, Create, Link, Shell])
+        for plugin_dir in options.plugin_dirs:
+            for plugin_path in glob.glob(os.path.join(plugin_dir, "*.py")):
+                plugin_paths.append(plugin_path)
+        for plugin in plugin_paths:
+            path = self._find_plugin_path(directory, plugin)
+            if path is None:
+                self._log.error(f"Could not find plugin. The path should be relative to the base directory")
+            else: 
+                load_plugins = module.load(path)
+                for plugin in load_plugins:
+                    if (not plugin.__name__ == Includes.__name__
+                        and not plugin in plugins):
+                        plugins.append(plugin)
+                        load_plugins.remove(plugin)
+        return plugins
+
+    def _find_plugin_path(self,
+                          directory: str,
+                          plugin: str):
+        current = directory
+        path = self._resolve_path(current, plugin)
+        if os.path.exists(path): return path
+        else: current = os.getcwd()
+        # Perhaps plugin path is somewhere relative to the working directory ?
+        while True:
+            path = self._resolve_path(current, plugin)
+            if os.path.exists(path): return path
+            else:
+                parent = os.path.dirname(current)
+                if current == parent:  # if current is root
+                    return None
+                else: current = parent
+
     def _resolve_path(self, parent: str, child: str):
-        return child if os.path.isabs(child) else os.path.join(parent, child)
+        return os.path.normpath(os.path.expanduser(child)) if os.path.isabs(child) else os.path.join(parent, child)
